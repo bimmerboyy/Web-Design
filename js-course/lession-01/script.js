71bcc4797779e5dac915ae53f83b783da269f187
bimmer <tarikkucevic@gmail.com>
2022-10-20 12:28:46 +0200

    js course first lesson

---
+++ js-course/lession-01/script.js
@@ -0,0 +1,11 @@
+// console.log("Pozdrav iz konzole")
+//document.write("Pozdrav sa stranice")
+// console.error("POzdrav iz konzole")
+// console.warn("POzdrav iz konzole")
+//window.open()
+document.getElementById('poruka').innerrText = 'Pozdrav!!'
+
+console.log(document.getElementById('poruka').innerrText);
+
+window.alert("Pozdrav iz Popup-a")
+console.log("B" + "A"++"A")
