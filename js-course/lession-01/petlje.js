7ad48bee122ed1e13bcb3136784183af37cc5220
bimmer <tarikkucevic@gmail.com>
2022-10-21 22:09:13 +0200

    first lesson from cysecor course

--- js-course/lession-01/petlje.js
+++ js-course/lession-01/petlje.js
@@ -24,16 +24,53 @@ let izbrojaneNovcanice = 0;
 let novcanice = [10, 5, 20, 50, 5, 10, 100, 20, 50, 5, 10, 20];
 
 let brojNovcanica = novcanice.length;
-let brojac = 0;
+// let brojac = 0;
 
-while (brojac < brojNovcanica) {
-    console.log("---------------");
-    console.log("Uslov prosao");
-    console.log(`Novacanica na poziciji [${brojac}]: ${novcanice[brojac]}`);
-    console.log(novcanice[brojac]);
-    brojac++;
-}
+// while (brojac < brojNovcanica) {
+//     console.log("---------------");
+//     console.log("Uslov prosao");
+//     console.log(`Novacanica na poziciji [${brojac}]: ${novcanice[brojac]}`);
+//     console.log(novcanice[brojac]);
+//     brojac++;
+// }
+// console.log("## KRAJ PETLJE ##")
+
+// if (brojac < brojNovcanica) {
+//     console.log("Brojac je manji od broja nnovacnica ");
+// } else {
+//     console.log("Brojac nije manji od broja novacnica ");
+
+// }
+// console.log(`brojac:${brojac}`);
 // while (izbrojaneNovcanice < brojNovcanica) {
 //     izbr ojaneNovcanice++;
 
 // }
+
+//for petlja
+
+// let ukupnoNovca = 0;
+
+// for (let brojac = 0; brojac < brojNovcanica; brojac++)
+//Moze isto da se napise for(let brojac in novcanice) ili bez brojaca
+//for(let novacnica in novcanice){
+// console.log(novcanica);
+//}
+// {
+//     console.log(novcanice[brojac]);
+//     ukupnoNovca += novcanice[brojac];
+// }
+// console.log("## KRAJ PETLJE ##")
+
+// console.log(`Ukupno imamo ${ukupnoNovca} eura.`)
+
+let autombili = ["Audi", "BMW", "Porsche", "Mercedes"];
+
+// for (let automobil of autombili) {
+//     console.log(automobil);
+// }
+
+autombili.forEach(function(automobil) {
+    console.log(automobil);
+
+})
