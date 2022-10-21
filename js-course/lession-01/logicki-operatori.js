cdb7885c176958f0047fbbdc14c8c047edb34000
bimmer <tarikkucevic@gmail.com>
2022-10-21 20:59:20 +0200

    first lesson from cysecor course

---
+++ js-course/lession-01/logicki-operatori.js
@@ -0,0 +1,13 @@
+let prviBroj = 10;
+let drugiBroj = 5;
+
+let terciBroj = 7;
+let cetvrtiBroj = 8;
+
+console.log(prviBroj > drugiBroj);
+console.log(terciBroj > cetvrtiBroj);
+
+console.log((prviBroj > drugiBroj) && (terciBroj > cetvrtiBroj))
+
+
+console.log((prviBroj > drugiBroj) || (terciBroj > cetvrtiBroj))
---
+++ js-course/lession-01/uslovi.js
@@ -0,0 +1,25 @@
+// let prviBroj = 5;
+// let drugiBroj = 10;
+
+// if (prviBroj < drugiBroj) {
+//     console.log(`${prviBroj} je manji od ${drugiBroj}`);
+// } else {
+//     console.log(`${prviBroj} nije manji od ${drugiBroj}`);
+
+// }
+
+//else if
+
+// let vrijeme = 21;
+
+// if (vrijeme < 10) {
+//     console.log("Dobro jutro!")
+// } else if (vrijeme < 20) {
+//     console.log("Dobar dan!")
+// } else {
+//     console.log("Dobro vece!")
+// }
+
+//switch
+
+let mjesec = 6;
