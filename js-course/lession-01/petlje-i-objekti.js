5ddba75a75b1cbbcd546ed3ed538cb47adc6f494
bimmer <tarikkucevic@gmail.com>
2022-10-31 13:20:04 +0100

    DUNP wd-4 exercise 1

---
+++ js-course/lession-01/petlje-i-objekti.js
@@ -0,0 +1,14 @@
+let automobili = {
+    audi: ["50000", "2017", "Q7"],
+    bmw: ["60000", "2018", "330d"],
+    porsche: ["70000", "2016", "Boxter"],
+    mercedes: ["80000", "2019", "S400"]
+};
+for (automobil in automobili) {
+    let podaciOvozilu = automobili[automobil];
+    let naziv = auutomobil;
+    console.log(naziv);
+    console.log(podaciOvozilu);
+    console.log("--------------------------------");
+}
+console.log(automobil);
