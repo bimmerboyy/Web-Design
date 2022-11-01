27a1318e33f69e6846d6b4699b3ba06ea9cf26bb
bimmer <tarikkucevic@gmail.com>
2022-11-01 18:50:58 +0100

    selecting HTML elements from js course first lesson

--- js-course/lession-01/script-for-selecting.js
+++ js-course/lession-01/script-for-selecting.js
@@ -17,13 +17,27 @@
 
 // console.log(najbolji.innerText);
 
-brend = document.querySelectorAll("span");
+// brend = document.querySelectorAll("span");
 
-let modeli = document.querySelectorAll(".automobil-model");
-for (let model of modeli) {
-    console.log(model.innerText);
-}
+// let modeli = document.querySelectorAll(".automobil-model");
+// for (let model of modeli) {
+//     console.log(model.innerText);
+// }
+
+// let recenica = document.querySelector("#recenica");
+
+// console.log(recenica.innerText);
+
+//Selektovanje elementa po ID-u i brisanje istog
 
 let recenica = document.querySelector("#recenica");
+recenica.remove(); //remove sluzi za brisanje elementa sa stranice
 
-console.log(recenica.innerText);
+//Kreiranje novog div elemena
+let noviEl = document.createElement("div");
+
+nociEl.classList = "novi-element";
+
+document.appendChild(noviEl);
+
+console.log(noviEl);
