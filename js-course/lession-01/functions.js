e948bd00bc1763b2c6a5f9c4b6b9c07b74e226ca
bimmer <tarikkucevic@gmail.com>
2022-11-01 16:11:46 +0100

    functions from js course,first lesson

--- js-course/lession-01/functions.js
+++ js-course/lession-01/functions.js
@@ -4,6 +4,28 @@ let nekretnine = {
     plac: "30000"
 };
 
+function racunanjeMjesecneRate(godine, vrstaNekretnine, ) {
+    let mjeseci = godine * 12;
+    let rata = nekretnine[vrstaNekretnine] / mjeseci;
+    rata = rata.toFixed(0);
+
+    console.log(rata);
+
+}
+//Proracun za stan na 15 godina
+racunanjeMjesecneRate(15, "stan");
+//Proracun za plac na 5 godina
+racunanjeMjesecneRate(5, "plac");
+//Proracun za kucu na 20 godina
+racunanjeMjesecneRate(20, "kuca");
+
+
+
+
+
+
+/*
+
 // STAN
 let godine = 10;
 let mjeseci = godine * 12; // npr hocemo da placamo na rate 10 godina
@@ -15,7 +37,7 @@ rata = rata.toFixed(0); //stavlja ratu na 0 decimala tj ceo broj
 
 console.log("mjeseci: " + mjeseci);
 console.log(rata + " eura");
-console.log(`Mjesecna rata na stan iznosi ${rata} eura za ${godine} goidna`);
+console.log(`Mjesecna rata na stan iznosi ${rata} eura za ${godine} goidna\n`);
 
 //----------------------------------------------------------------
 
@@ -23,3 +45,24 @@ console.log(`Mjesecna rata na stan iznosi ${rata} eura za ${godine} goidna`);
 
 godine = 5;
 mjeseci = godine * 12;
+rata = nekretnine.plac / mjeseci;
+
+rata = rata.toFixed(0);
+
+console.log("----------------------------------------------------------------");
+console.log(`\nMjesecna rata na plac iznosi ${rata} eura za ${godine} goidna`);
+
+//----------------------------------------------------------------
+
+// KUCA
+
+godine = 20;
+mjeseci = godine * 12;
+rata = nekretnine.kuca / mjeseci;
+
+rata = rata.toFixed(0);
+
+console.log("----------------------------------------------------------------");
+console.log(`\nMjesecna rata na kucu iznosi ${rata} eura za ${godine} goidna`);
+
+*/
