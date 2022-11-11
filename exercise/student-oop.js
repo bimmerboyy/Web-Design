bb0fd94cdbe3a0267b69417fc46352717b08c1ef
bimmer <tarikkucevic@gmail.com>
2022-11-11 19:49:42 +0100

    DUNP OOP exercise 5

---
+++ exercise/student-oop.js
@@ -0,0 +1,18 @@
+  /*
+                       Kreirati kalsu Student koja ima atribute ime,prezime,brojIndeksa,ocene
+                       metode:
+                       konstruktor sa argumrntima (i,p,bi)
+                       predstaviSe() - vraca prosek studenta
+                       */
+
+  class Student {
+      constructor(i, p, bi) {
+          this.ime = i;
+          this.prezime = p;
+          this.brojIndeksa = bi;
+          this.ocene = [];
+      }
+      predstaviSe() {
+          for ()
+      }
+  }
