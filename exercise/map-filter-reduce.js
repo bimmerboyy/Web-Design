eeccfd21cb91487fc3245790038a7f349039a4bc
bimmer <tarikkucevic@gmail.com>
2022-11-11 20:34:06 +0100

    exercise filter map and reduce functions

--- exercise/map-filter-reduce.js
+++ exercise/map-filter-reduce.js
@@ -13,4 +13,18 @@ function binary(x) {
 }
 const output = arr.map(binary);
 
-console.log(output);
+// console.log(output);
+
+//filter odd numbers
+
+function isOdd(x) {
+    return x % 2;
+}
+
+function isEven(x) {
+    return x % 2 === 0;
+}
+
+const output2 = arr.filter(isEven);
+
+console.log(output2);
