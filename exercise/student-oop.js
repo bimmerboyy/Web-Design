9e102faf14543fc082ed15c97014ec1d5b959e1e
bimmer <tarikkucevic@gmail.com>
2022-11-11 20:01:35 +0100

    oop exercises GPA of students

--- exercise/student-oop.js
+++ exercise/student-oop.js
@@ -1,9 +1,9 @@
   /*
-                       Kreirati kalsu Student koja ima atribute ime,prezime,brojIndeksa,ocene
-                       metode:
-                       konstruktor sa argumrntima (i,p,bi)
-                       predstaviSe() - vraca prosek studenta
-                       */
+                                       Kreirati kalsu Student koja ima atribute ime,prezime,brojIndeksa,ocene
+                                       metode:
+                                       konstruktor sa argumrntima (i,p,bi)
+                                       predstaviSe() - vraca prosek studenta
+                                       */
 
   class Student {
       constructor(i, p, bi) {
@@ -13,6 +13,26 @@
           this.ocene = [];
       }
       predstaviSe() {
-          for ()
+          console.log(`Moje ime je ${this.ime} ${this.prezime} i broj indeksa ${this.brojIndeksa}`);
       }
+      prosek() {
+              let s = 0;
+              let duzina = this.ocene.length;
+              for (let i = 0; i < duzina; i++) {
+                  s += Number(ocene[i]);
+              }
+              return s / duzina;
+          }
+          //   prosek() {
+          //       let s = 0;
+          //       this.ocene.forEach(ocena => {
+          //           s += Number(ocene[i]);
+          //       })
+          //       return s / duzina;
   }
+
+  const student1 = new Student("Nikola", "Matkovic", 123456);
+  const stringOcene = prompt("Unesite ocene studenta:");
+  student1.ocene = stringOcene.split(" ");
+  student1.predstaviSe();
+  console.log(`Student ima prosek ${prosek()}`);
