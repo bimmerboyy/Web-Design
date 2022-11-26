fb1f0c8be82004ec8dc227f96ae60b24df1e5e4d
bimmerboyy <tarikkucevic@gmail.com>
2022-11-26 23:07:50 +0200

    pattern 1 exercise

--- exercise/pattern01.js
+++ exercise/pattern01.js
@@ -8,15 +8,14 @@
 let n = prompt("Unesite n:");
 
 let pattern = n =>{
-    for(let row = 0; row < n; row++){ //Prvo gledamo koliko linija imamo u samom patternu 
-         //za svaki red vazi toliko kolona
-         for(col = 1;col <= row;col++){
+    for(let row = 1; row < n; row++){ //Prvo gledamo koliko linija imamo u samom patternu 
+        //za svaki red vazi toliko kolona
+        for(col = 1;col <= row;col++){
            document.write("* ");
-         }
-         //kada smo ispisali jedan red stavljamo novi red <br>
-         document.write("<br>");
+        }
+        //kada smo ispisali jedan red stavljamo novi red <br>
+        document.write("<br>");
     }
 }
-
 
 doucument.write(pattern(4));
