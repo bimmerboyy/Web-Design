ca12c1fab47f6e96610ae5c50f882acb1a083c25
bimmerboyy <tarikkucevic@gmail.com>
2022-11-26 22:18:51 +0200

    ok

---
+++ exercise/pattern01.js
@@ -0,0 +1,15 @@
+/*
+*
+**
+***
+****
+*/
+
+let pattern = n =>{
+    for(let row = 0; row < n; row++){ //Prvo gledamo koliko linija imamo u samom patternu 
+         //za svaki red vazi toliko kolona
+         for(col = 1;col <= row;col++){
+           
+         }
+    }
+}
