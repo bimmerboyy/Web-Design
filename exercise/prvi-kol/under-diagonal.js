5a7a9e838ef9194019a75b12919ff439e261b616
bimmerboyy <tarikkucevic@gmail.com>
2022-11-27 23:08:32 +0200

    ok

---
+++ exercise/prvi-kol/under-diagonal.js
@@ -0,0 +1,7 @@
+/*Napisati program koji odredjuje srednju vrednost elemenata ispod glavne dijagonale kvadratne matrice */
+
+let matrix = math.matrix([1,2,3],[4,5,6],[7,8,9]);
+
+let odredjivanjeSrednjeVrednosti = matrix =>{
+    
+};
