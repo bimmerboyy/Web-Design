2bb4cb58247e52e2ebb8b652c2da2018f54e51b9
bimmerboyy <tarikkucevic@gmail.com>
2022-11-28 00:14:30 +0200

    averge value of elements under main diagonal

--- exercise/prvi-kol/under-diagonal.js
+++ exercise/prvi-kol/under-diagonal.js
@@ -1,7 +1,18 @@
 /*Napisati program koji odredjuje srednju vrednost elemenata ispod glavne dijagonale kvadratne matrice */
 
-let matrix = math.matrix([1,2,3],[4,5,6],[7,8,9]);
+let matrix = [[1,2,3],[4,5,6],[7,8,9]];
 
 let odredjivanjeSrednjeVrednosti = matrix =>{
-    
+    let srednjaVrednost = 0;
+    let brojac = 0;
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[i].length; j++) {
+           if(i > j){
+            srednjaVrednost += matrix[i][j];
+            brojac++;
+           }
+        }
+    }
+    return srednjaVrednost/brojac;
 };
+console.log(odredjivanjeSrednjeVrednosti(matrix));
