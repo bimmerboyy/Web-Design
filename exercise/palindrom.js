3f1424b5235570deced8afbf6af103223005b468
bimmerboyy <tarikkucevic@gmail.com>
2022-11-27 17:09:20 +0200

    check is array of charcters palindrome exercise

---
+++ exercise/palindrom.js
@@ -0,0 +1,27 @@
+//Palindorm
+
+//Npr ABCDCBA madam
+
+// anavolimilovana
+
+
+let palindrom = prompt("Unesite niz karaktera");
+
+let daLiJePalindrom = (...palindrom) =>{
+    for(let i = 0;i < palindrom.length/2;i++){
+        let prviNiz = palindrom[i];
+        let zajednicki1 = palindrom[(palindrom.length/2) + 1];
+    }
+    for(let i = (palindrom.length/2) + 2;i < palindrom.length;i++){
+        let drugiNiz = palindrom[i];
+        let zajednicki2 = [(palindrom.length/2) + 1];
+    }
+    for(let i = 0;i < palindrom.length;i++){
+        if(prviNiz[i] == drugiNiz[i] && zajednicki1 == zajednicki2){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
