10587cab40b215a8c94596411f7af2e8cc72cccd
bimmerboyy <tarikkucevic@gmail.com>
2022-11-29 20:52:38 +0200

    check if equal

---
+++ exercise/prvi-kol/jednakost.js
@@ -0,0 +1,18 @@
+let broj1 = 10;
+let broj2 = "10";
+if(broj1 == broj2){
+    console.log("Jednaki su sa dva!");
+}
+if(broj1 === broj2){
+    console.log("Jednaki su sa tri!");
+}
+
+let tacno1 = 0;
+let tacno2 = false;
+
+if(tacno1 == tacno2){
+    console.log("Jednaki su sa dva!");
+}
+if(tacno1 === tacno2){
+    console.log("Jednaki su sa tri!");
+}
