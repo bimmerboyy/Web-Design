9bca2e5c1b3b752ebdcaa84636725e313f4bc39c
bimmerboyy <tarikkucevic@gmail.com>
2023-01-04 15:35:38 +0200

    second lesson js course exercise 1

--- js-course/lesson-02/script.js
+++ js-course/lesson-02/script.js
@@ -1,3 +1,10 @@
-function pokreniMe(){
-    console.log("Klik!!");
+function pokreniMe(dugme){
+    let nazivDugmeta = dugme.innerText;
+    let klasaDugmeta = dugme.className;
+
+    console.log(klasaDugmeta);
+
+    let message = document.querySelector("#kliknutoDugme");
+     message.innerText = nazivDugmeta + " " + klasaDugmeta;
+    console.log(dugme);
 }
