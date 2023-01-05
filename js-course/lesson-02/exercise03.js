0846ca8eac2b78e8f8c33ab852406780bd790fb3
bimmer <tarikkucevic@gmail.com>
2023-01-05 14:35:35 +0100

    js course lesson 2 exercise 3

--- js-course/lesson-02/exercise03.js
+++ js-course/lesson-02/exercise03.js
@@ -1,3 +1,23 @@
-function testing() {
+function validacija() {
+    let input = document.querySelector("#email");
+    let value = input.value;
+    if (value.includes("@") && value.includes(".")) {
+        console.log("validan mejl");
 
+        let pozicijaAt = value.indexOf("@");
+        let pozicijaTacka = value.indexOf(".");
+        let izmedjuAtTacka = value.substring(pozicijaAt + 1, pozicijaTacka);
+
+
+        if (izmedjuAtTacka.length > 0) {
+            console.log("Dobro je");
+        } else {
+            console.log("Nije dobro");
+        }
+
+
+
+    } else {
+        console.log("Mejl nije validan");
+    }
 }
