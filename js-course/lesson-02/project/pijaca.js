81b4943310010ff8fdf94d9d01bd9b13e212369f
bimmer <tarikkucevic@gmail.com>
2023-01-05 18:42:22 +0100

    ok

--- js-course/lesson-02/project/pijaca.js
+++ js-course/lesson-02/project/pijaca.js
@@ -4,9 +4,29 @@ function addToCart(element) {
     let price = mainEl.querySelector(".price").innerText;
     let name = mainEl.querySelector("h3").innerText;
     let quantity = mainEl.querySelector("input").value;
-    let cartItems = document.querySelectorAll(".cart-items");
+    let cartItems = document.querySelector(".cart-items");
+    console.log(typeof(quantity));
+    if (parseInt(quantity) > 0) {
+        price = price.substring(1);
+        price = parseInt(price);
+        let total = price * parseInt(quantity);
 
-    cartItems.innerHTML = "Dodato " + name;
+
+        cartItems.innerHTML += `<div class="cart-single-item">
+            <h3>${name}</h3>
+            <p>$${price} x ${quantity} = $${total}</p>
+
+        </div>`
+
+
+        element.innerText = "Dodato";
+        element.setAttribute("disabled", "true");
+
+    } else {
+        alert("Odaberi kolicinu")
+    }
+
+
 
 
 
