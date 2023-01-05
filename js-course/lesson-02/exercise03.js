05c5b3b55731e3a55a8cb8bc30adea30f6c524ca
bimmer <tarikkucevic@gmail.com>
2023-01-05 14:46:54 +0100

    js course lesson 2 exercise 3

--- js-course/lesson-02/exercise03.js
+++ js-course/lesson-02/exercise03.js
@@ -1,19 +1,28 @@
 function validacija() {
     let input = document.querySelector("#email");
     let value = input.value;
+    //Provjera da li value (string) sarzi @ i tacku
     if (value.includes("@") && value.includes(".")) {
-        console.log("validan mejl");
 
-        let pozicijaAt = value.indexOf("@");
-        let pozicijaTacka = value.indexOf(".");
+
+        let pozicijaAt = value.indexOf("@"); //uzimamo poziciju @
+        let pozicijaTacka = value.indexOf("."); // uzimamo poziciju tacke
+
+        //uzima vrijednost izmedju pozciija. Ne ukljucuje poslednju poziciju
         let izmedjuAtTacka = value.substring(pozicijaAt + 1, pozicijaTacka);
 
-
+        // da li izmedju tacke i @ imamo vise od nula karaktera
         if (izmedjuAtTacka.length > 0) {
             let prijeAt = value.substring(0, pozicijaAt);
+            // da li prije @ imamo vise od dva karaktera
             if (prijeAt.length > 2) {
                 let posleTacke = value.substring(pozicijaTacka + 1, value.length);
-                console.log(posleTacke);
+                //da li posle tacke imamo vise od 1 karaktera
+                if (posleTacke.length > 1) {
+                    console.log("Email je validan!");
+                } else {
+                    console.log("Mail nije validan");
+                }
             } else {
                 console.log("Mejl nije validan");
             }
