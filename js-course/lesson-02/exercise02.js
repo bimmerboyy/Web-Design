4134254a4ce42714296dab635d05745d3df3d911
bimmer <tarikkucevic@gmail.com>
2023-01-05 14:02:34 +0100

    js course lesson 2 exercise 2

--- js-course/lesson-02/exercise02.js
+++ js-course/lesson-02/exercise02.js
@@ -3,13 +3,22 @@
 //     console.log(linkovi[0].innerText);
 // }
 
-function testing(){
-    let link2 = document.querySelector(".drugi-link");
-    let link1 = link2.previousElementSibling;
-    let link3 = link2.nextElementSibling;
+function testing() {
+    // let link2 = document.querySelector(".drugi-link");
+    // let link1 = link2.previousElementSibling;
+    // let link3 = link2.nextElementSibling;
 
     let link = document.querySelector(".drugi-link");
-    console.log(link.closest("div"));
+    // console.log(link.closest("div"));
+    let gl_div = document.querySelector(".glavni-div");
+
+    gl_div.innerHTML = "<span>Klikni ovaj link: <a>Klik</a></span>"
+
+    // link.setAttribute("href", "http://google.com/");
+    // link.setAttribute("class", "plavo-dugme");
+    // console.log(link.getAttribute("class"));
+    // link.removeAttribute("data-test");
+
     // console.log(link1.innerText);
 
     // console.log(link2.innerText);
