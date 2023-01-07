b8fe275db89424ebcabefd3c7b5cafcc1df5c6be
bimmer <tarikkucevic@gmail.com>
2023-01-07 14:32:01 +0100

    homework from js course lesson 2

--- js-course/domaci-zadaci/filmoteka/domaci02.js
+++ js-course/domaci-zadaci/filmoteka/domaci02.js
@@ -1,19 +1,47 @@
 function watchMovie() {
     console.log("Radi");
 }
+let trenutniBroj = 0;
+
+
+
 
 function rateFilm() {
     console.log("Radi ocena");
+
     let numberOfGrades = document.querySelector("input").value;
-    console.log(numberOfGrades);
-
-    for (let i = numberOfGrades; i <= numberOfGrades; i++) {
-
-        let star = document.createElement("i");
-        star.setAttribute("class", "fa-solid fa-star");
-        let grades = document.querySelector(".grades");
-        grades.appendChild(star);
+    if (trenutniBroj <= numberOfGrades) {
+        trenutniBroj = numberOfGrades;
 
     }
+
+
+
+
+    if (numberOfGrades > 0) {
+        if (numberOfGrades == 5) {
+            let input = document.querySelector("input");
+            input.remove();
+        }
+        if (trenutniBroj > numberOfGrades) {
+            star.remove();
+        }
+
+
+        for (let i = numberOfGrades; i <= numberOfGrades; i++) {
+
+            let star = document.createElement("i");
+            star.setAttribute("class", "fa-solid fa-star");
+            let grades = document.querySelector(".grades");
+            grades.appendChild(star);
+
+
+
+        }
+    }
+
+
+
+
 
 }
