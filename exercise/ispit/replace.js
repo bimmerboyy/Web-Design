ec4178766490db0d204a04e445c75b1421c3c65a
bimmer <tarikkucevic@gmail.com>
2023-01-08 14:18:32 +0100

    replace method exercise

--- exercise/ispit/replace.js
+++ exercise/ispit/replace.js
@@ -1,6 +1,6 @@
 //Zameniti uzorak u stringu
 
-let uzorak1 = /HTML/g // g - globalna primena - vrsi se zamena svakog uzorka koji se nadje u okviru stringa
+let uzorak1 = /HTML/g // g - globalna primena - vrsi se zamena svakog uzorka koji se nadje u okviru stringa.
 let tekst1 = "HTML nije programski jezik";
 
 let zamena1 = tekst1.replace(uzorak1, "JAVA");
