abf57aa32979f8c06867d7e6f0b591aa7ea81312
bimmer <tarikkucevic@gmail.com>
2023-01-08 18:52:59 +0100

    js course lesson 3 arow functions

---
+++ js-course/lesson03/script.js
@@ -0,0 +1,17 @@
+function klikni_me(a, b) {
+    // let a = 5;
+    // let b = 10;
+    console.log(a + b);
+}
+
+klikni_me(10, 15);
+
+//-----------------------
+
+const klikni_me_nova = (a, b) => console.log(a + b);
+// let a = 5;
+// let b = 10;
+
+
+
+klikni_me_nova(100, 500);
