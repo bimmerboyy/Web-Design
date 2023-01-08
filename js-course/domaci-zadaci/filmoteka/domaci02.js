0e2f5264db2f3dcfad4a10fd86d3d9a7e0d62450
bimmer <tarikkucevic@gmail.com>
2023-01-08 11:30:32 +0100

    homework from js course lesson 2

--- js-course/domaci-zadaci/filmoteka/domaci02.js
+++ js-course/domaci-zadaci/filmoteka/domaci02.js
@@ -11,8 +11,8 @@ function watchMovie(element) {
     let chart = document.querySelector(".wached-films");
     chart.innerHTML += `<div class="first">
         <p class="name-of-film">${name}</p>
-        <p class="price-of-film">$${price}</p>
-        <button onclick="removeFromCart">Ukloni</button>
+        <p class="price-of-film">$<span>${price}</span></p>
+        <button onclick="removeFromCart(this)">Ukloni</button>
         </div>
         `;
     let total = document.querySelector(".total p span");
@@ -22,6 +22,28 @@ function watchMovie(element) {
 
 
 }
+
+function removeFromCart(element) {
+    let mainEl = element.closest(".first");
+    let price = mainEl.querySelector(".price-of-film span").innerText;
+    price = parseInt(price);
+    let name = mainEl.querySelector(".name-of-film").innerText;
+    allTotal -= price;
+    let total = document.querySelector(".total span").innerHTML = allTotal + "$";
+    mainEl.remove();
+
+    let films = document.querySelectorAll(".card");
+    films.forEach(function(film) {
+        let filmName = film.querySelector("h3").innerText;
+        if (filmName === name) {
+            film.querySelector(".gledaj").removeAttribute("disabled");
+            film.querySelector(".gledaj").innerText = "Gledaj";
+
+        }
+
+    });
+}
+
 
 
 
