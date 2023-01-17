169d261de9ee9571fdcfd87d216b22488e97704e
bimmer <tarikkucevic@gmail.com>
2023-01-17 14:55:39 +0100

    webpack files

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,17 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
     },
-    devServer
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
     module: {
         rules: [{
             test: /\.scss$/,
