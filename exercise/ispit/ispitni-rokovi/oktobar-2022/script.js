c836570730bedd04095e6cf3a30adfa74278e6a9
bimmer <tarikkucevic@gmail.com>
2023-01-25 13:12:48 +0100

    making game with cards

---
+++ exercise/ispit/ispitni-rokovi/oktobar-2022/script.js
@@ -0,0 +1,25 @@
+/*[30] Kreirati JavaScript kod pomoću kojeg će se ilustrovati igra memorije. Igra se sastoji
+od 16 kartica postavljenih kao mreža ili tablica, kod kojih svaka karta ima odgovarajući par.
+Sve kartice su prvobitno okrenute i vidljiva im je poledina (koja je ista kod svih).
+Random funkcijom kartama su dodeljena slova (8 različitih slova). Karte su položene
+na podlogu "licem" tako da su sva slova skrivena. Klikom na kartu ona se okreće i vidi
+se slovo koje se nalazi na njoj. Možemo otvoriti jos jednu karticu i ako su slova ista one
+se brišu a ako nisu vraćaju se okrenute ka podlozi. Kraj igre je kad su sve kartice izbrisane.*/
+
+
+let filpCard = () => {
+    let cards = document.querySelectorAll('.card');
+    let removeImage = document.querySelectorAll('.card img');
+    removeImage.forEach((images, index) => {
+        images.src = "";
+        images.alt = index;
+
+    });
+    cards.forEach(card => {
+
+    });
+
+
+};
+
+filpCard();
