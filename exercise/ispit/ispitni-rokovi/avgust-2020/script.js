fd6566a7748d04f0ba5ccf26044ac028cf4a8552
bimmer <tarikkucevic@gmail.com>
2023-01-31 23:40:20 +0100

    finished site with data and pictures about cars

--- exercise/ispit/ispitni-rokovi/avgust-2020/script.js
+++ exercise/ispit/ispitni-rokovi/avgust-2020/script.js
@@ -12,12 +12,19 @@ Funkcionalnost dugmeta realizovati pomoću dve funkcije i globalnih atributa (ko
 U veb stranu proizvoljnog izgleda učitati eksternu JS datoteku koja sadrži klasu Automobil, programskim putem dodati 10
 automobila u niz. Svaki automobil treba imati bar 2 slike.*/
 
+let nizAutomobila = [];
+
 class Automobil {
-    constructor(sifra, naziv, URL, cena, kilometraza, snaga, boja) {
+
+
+    constructor(sifra, naziv, URL1, URL2, cena, kilometraza, snaga, boja) {
         this.sifra = sifra;
         this.naziv = naziv;
         this.image = new Image();
-        this.image.URL = URL;
+        this.image.URL1 = URL1;
+        this.image.URL2 = URL2;
+        this.image2 = new Image();
+        this.image2.URL2 = URL2;
         this.cena = cena;
         this.kilometraza = kilometraza;
         this.snaga = snaga;
@@ -39,8 +46,11 @@ class Automobil {
         dg.appendChild(leva);
         dg.appendChild(desna);
         document.body.appendChild(dg);
+
         let image = document.createElement('img');
-        image.src = this.image.URL;
+        let image2 = document.createElement('img');
+        image.src = this.image.URL1;
+
         image.style.width = "100%";
         let levaStrana = document.querySelector('.dg .leva');
         levaStrana.appendChild(image);
@@ -50,7 +60,10 @@ class Automobil {
         <br/> Cena:${this.cena} <br/> Kilometraza:${this.kilometraza} <br/> Snaga:${this.snaga} <br/> Boja:${this.boja}`;
         let desnaStrana = document.querySelector('.dg .desna');
         desnaStrana.appendChild(p);
+
         //kreiranje buttona
+
+
         let prethodna = document.createElement('button');
         let sledeca = document.createElement('button');
         prethodna.textContent = 'Prethodna';
@@ -59,13 +72,48 @@ class Automobil {
 
         document.body.appendChild(prethodna);
         document.body.appendChild(sledeca);
+
+        prethodna.addEventListener('click', promeniUnazad);
+        sledeca.addEventListener('click', promeniUnapred);
+
+        let sifraAuta = this.sifra;
+        let drugaSlika = this.image.URL2;
+        let nizSlika = [];
+
+        function promeniUnazad() {
+
+            for (let i = 0; i < nizAuta.length; i++) {
+                if (nizAuta[i].sifra === sifraAuta) {
+                    image.remove();
+                    nizAuta[i].image2.setAttribute('src', drugaSlika);
+                    nizAuta[i].image2.style.width = "900px";
+                    levaStrana.appendChild(nizAuta[i].image2);
+                }
+
+            }
+        }
+
+        function promeniUnapred() {
+
+            for (let i = 0; i < nizAuta.length; i++) {
+
+                if (nizAuta[i].sifra === sifraAuta) {
+
+                    nizAuta[i].image2.remove();
+                    levaStrana.appendChild(image);
+                }
+
+            }
+        }
     }
-
-
 
 }
 
-let auto1 = new Automobil(1, 'BMW', '../avgust-2020/BMW-M3-E30-Redux.jpg', '100000', '35000', 280, 'crna');
+let auto1 = new Automobil(1, 'BMW', '../avgust-2020/BMW-M3-E30-Redux.jpg', '../avgust-2020/1-BMW-E30-M3-by-Redux.jpg', '100000', '35000', 280, 'crna');
+let auto2 = new Automobil(2, 'Mercedes', '../avgust-2020/auto2.jpg', '../avgust-2020/s-klasa.jpg', '130000', '15000', 450, 'siva');
+let auto3 = new Automobil(3, 'Audi', '../avgust-2020/audi-a6-1.jpg', '../avgust-2020/audi-a6-2.jpg', '70000', '120000', 420, 'plava');
+let nizAuta = [auto1, auto2, auto3];
 
 
-auto1.prikazPodataka();
+
+auto3.prikazPodataka();
